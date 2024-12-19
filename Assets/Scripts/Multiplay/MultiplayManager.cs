1e01d6b018b8cf7e7edc2bd49fa3061bb14cd5ac
bubblestew95 <bubblestew95@gmail.com>
2024-12-19 15:32:47 +0900

    멀티플레이 매니저 생성, 테스트 플레이어와 Multiplayer Play Mode 패키지 추가.

--- Assets/Scripts/Multiplay/MultiplayManager.cs
+++ Assets/Scripts/Multiplay/MultiplayManager.cs
@@ -1,16 +1,42 @@
 using UnityEngine;
+using Photon.Pun;
 
-public class MultiplayManager : MonoBehaviour
+public class MultiplayManager : MonoBehaviourPun
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField]
+    private string gameVersion = "0.0.1";
+
+    private const int MaxPlayerPerRoom = 2;
+
+    #region Public Func
+
+    /// <summary>
+    /// 방에 접속중인 플레이어들에게 새로운 씬으로 가도록 함.
+    /// </summary>
+    /// <param name="_sceneName">로드할 씬 이름</param>
+    public void LoadScene(string _sceneName)
     {
-        
+        PhotonNetwork.LoadLevel(_sceneName);
     }
 
-    // Update is called once per frame
-    void Update()
+    #endregion
+
+    #region Private Func
+
+    #endregion
+
+    #region Unity Callback Func
+
+    private void Awake()
     {
-        
+        // 게임 시작 시 두 플레이어는 항상 같은 씬에 있어야 한다.
+        // 씬을 자동으로 동기화하도록 설정.
+        PhotonNetwork.AutomaticallySyncScene = true;
     }
+
+    #endregion
+
+    #region Photon Callback Func
+
+    #endregion
 }
