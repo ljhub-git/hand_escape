b8f3b58784803f1d4cbf2cd583eb7320a542dc6f
lordcreep <hjgr.lordcreep@gmail.com>
2024-12-23 09:32:52 +0900

    심플 인벤토리. 아이템 및 리스트 수정 필요

--- Assets/Scripts/Utilities/Inventory/InventoryUI.cs
+++ Assets/Scripts/Utilities/Inventory/InventoryUI.cs
@@ -3,30 +3,80 @@ using UnityEngine;
 
 public class InventoryUI : MonoBehaviour
 {
+    public static InventoryUI instance;
+
     [SerializeField]
     private Transform invenContentTr = null;
     [SerializeField]
     private GameObject invenItemPrefab = null;
 
+    private List<InventoryItem> inventoryItems = new List<InventoryItem>();
 
-    private void OnTriggerEnter(Collider other)
+    private void Awake()
     {
-        // 충돌한 객체에 Item_Test 컴포넌트가 있는지 확인
-        Item_Test item = other.GetComponent<Item_Test>();
-
-        if (item != null && item.isBeingDragged)
+        if (instance == null)
         {
-            // 충돌한 객체가 Item_Test를 가지고 있으면 인벤토리에 아이템을 추가
-            SpawnInvenItem(item);
-            other.gameObject.SetActive(false);
+            instance = this; // Singleton 설정
+        }
+        else
+        {
+            Destroy(gameObject); // 이미 존재하는 경우 삭제
         }
     }
 
-    public void SpawnInvenItem(Item_Test _itemInfo)
+    private void OnDestroy()
     {
-        // Instantiate 후 기본 설정
+        if (instance == this) instance = null;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Items"))
+        {
+            // 충돌한 객체에 Item_Test 컴포넌트가 있는지 확인
+            Item_Test item = other.GetComponent<Item_Test>();
+
+            if (item != null && item.isBeingDragged)
+            {
+                // 충돌한 객체가 Item_Test를 가지고 있으면 인벤토리에 아이템을 추가
+                item.isBeingDragged = false;
+                //SpawnInvenItem(item);
+                AddItemToInventory(item);
+                other.gameObject.SetActive(false);
+            }
+        }
+    }
+
+    public void AddItemToInventory(Item_Test itemInfo)
+    {
+        // 아이템 UI 생성
         GameObject invenItemGo = Instantiate(invenItemPrefab, invenContentTr);
-        // InventoryItem 컴포넌트 초기화
-        invenItemGo.GetComponent<InventoryItem>().Init(_itemInfo);
+        InventoryItem inventoryItem = invenItemGo.GetComponent<InventoryItem>();
+
+        // 초기화
+        inventoryItem.Init(itemInfo);
+
+        // 리스트에 아이템 추가
+        inventoryItems.Add(inventoryItem);
+    }
+
+    public void RemoveItemFromInventory(InventoryItem inventoryItem)
+    {
+        if (inventoryItems.Contains(inventoryItem))
+        {
+            inventoryItems.Remove(inventoryItem);
+            Destroy(inventoryItem.gameObject); // UI에서 아이템 제거
+        }
+    }
+
+    public void SpawnItemInScene(InventoryItem inventoryItem)
+    {
+        if (inventoryItem != null && inventoryItem.ItemInfo != null)
+        {
+            // 씬으로 아이템을 배치하고, UI에서 삭제
+            inventoryItem.ItemInfo.gameObject.SetActive(true);
+            //inventoryItem.ItemInfo.transform.position = new Vector3(0, 1, 0); // 원하는 위치에 배치
+            RemoveItemFromInventory(inventoryItem); // UI에서 아이템 삭제
+        }
     }
 }
