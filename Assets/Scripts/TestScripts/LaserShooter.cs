e6821872388e4e64782ca63aeedb8b49980a8ff1
bubblestew95 <bubblestew95@gmail.com>
2024-12-24 14:32:52 +0900

    Mirror 태그가 달린 콜라이더에 레이더 충돌 시 반사되는 효과 추가.

--- Assets/Scripts/TestScripts/LaserShooter.cs
+++ Assets/Scripts/TestScripts/LaserShooter.cs
@@ -19,47 +19,34 @@ public class LaserShooter : MonoBehaviour
 
     private void Update()
     {
-        CastLaser(transform.position, transform.forward);
+        CastLaser(startPoint.position, startPoint.forward);
     }
 
     private void CastLaser(Vector3 _pos, Vector3 _dir)
     {
         lr.SetPosition(0, startPoint.position);
 
-        Ray ray = new Ray(_pos, _dir);
-        RaycastHit hit;
-
-        if (Physics.Raycast(ray, out hit, 300))
+        for (int i = 0; i < maxBounces; ++i)
         {
-            _pos = hit.point;
-            _dir = Vector3.Reflect(_dir, hit.normal);
-            lr.SetPosition(1, hit.point);
+            Ray ray = new Ray(_pos, _dir);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, 15f))
+            {
+                _pos = hit.point;
+                _dir = Vector3.Reflect(_dir, hit.normal);
+                lr.SetPosition(i + 1, hit.point);
+
+                if(!hit.collider.CompareTag("Mirror"))
+                {
+                    break;
+                }
+            }
+            else
+            {
+                lr.SetPosition(i + 1, _pos + _dir * 15f);
+                break;
+            }
         }
-        else
-        {
-            lr.SetPosition(1, startPoint.position + _dir * 300f);
-        }
-
-        //for (int i = 0; i < maxBounces; ++i)
-        //{
-        //    Ray ray = new Ray(_pos, _dir);
-        //    RaycastHit hit;
-
-        //    if(Physics.Raycast(ray, out hit, 300, 1))
-        //    {
-        //        _pos = hit.point;
-        //        _dir = Vector3.Reflect(_dir, hit.normal);
-        //        lr.SetPosition(i + 1, hit.point);
-
-        //        if(hit.transform.name != "Mirror" && reflectOnlyMirror)
-        //        {
-        //            for(int j = i + 1; j <= maxBounces; ++j)
-        //            {
-        //                lr.SetPosition(j, hit.point);
-        //            }
-        //            break;
-        //        }
-        //    }
-        //}
     }
 }
