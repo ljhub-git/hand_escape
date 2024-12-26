c0872d7ce7905a2f3b7d61b539c9f74b8e3dd86c
bubblestew95 <bubblestew95@gmail.com>
2024-12-26 11:20:25 +0900

    레이저 계속 테스트중....

--- Assets/Scripts/Puzzle/Laser/LaserPuzzle.cs
+++ Assets/Scripts/Puzzle/Laser/LaserPuzzle.cs
@@ -10,6 +10,9 @@ public class LaserPuzzle : MonoBehaviour, IPuzzleObject
     private int maxBounces = 20;
     private float maxDistance = 15f;
 
+    private Vector3 pos = Vector3.zero;
+    private Vector3 dir = Vector3.zero;
+
     public void SolvePuzzle()
     {
         Debug.Log("SolvePuzzle");
@@ -26,29 +29,35 @@ public class LaserPuzzle : MonoBehaviour, IPuzzleObject
 
     private void FixedUpdate()
     {
-        CastLaser(startPoint.position, startPoint.forward);
+        CastLaser();
     }
 
-    private void CastLaser(Vector3 _pos, Vector3 _dir)
+    private void CastLaser()
     {
-        lr.SetPosition(0, _pos);
+        pos = startPoint.position;
+        dir = startPoint.forward;
+
+        lr.SetPosition(0, pos);
 
         // 최대 반사 가능한 횟수만큼 반복하면서 레이캐스트를 시행한다.
         for (int i = 0; i < maxBounces; ++i)
         {
-            Ray ray = new Ray(_pos, _dir);
+            Ray ray = new Ray(pos, dir);
             RaycastHit hit;
 
-            // 레이가 어떤 물체에 맞을 경우
+            Debug.DrawRay(pos, dir * maxDistance, Color.yellow);
+
+            // 레이가 어떤 물체에 맞은 경우
             if (Physics.Raycast(ray, out hit, maxDistance))
             {
-                _pos = hit.point;
+                pos = hit.point;
                 // 충돌한 지점의 노말을 이용해서 반사 효과를 일으킨다.
-                _dir = Vector3.Reflect(_dir, hit.normal);
+                dir = Vector3.Reflect(dir, hit.normal);
                 // 다음 시작 지점은 충돌한 지점.
-                lr.SetPosition(i + 1, _pos);
+                lr.SetPosition(i + 1, pos);
 
                 // 해당 물체가 레이저의 목표 골이라면 퍼즐이 풀린다.
+                // 레이저의 목표에 도달했으니까 레이저 반사를 더 안 해도 됨.
                 if (hit.collider.GetComponent<LaserGoal>() != null)
                 {
                     SolvePuzzle();
@@ -70,7 +79,7 @@ public class LaserPuzzle : MonoBehaviour, IPuzzleObject
                 lr.positionCount = i + 2;
 
                 // 아무 물체에 안 맞았을 경우 라인 방향에서 최대거리만큼 간 뒤 끊는다.
-                lr.SetPosition(i + 1, _pos + _dir * maxDistance);
+                lr.SetPosition(i + 1, pos + dir * maxDistance);
 
                 break;
             }
