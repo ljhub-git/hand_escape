975682e756cae812c6cb88d0311132fff11785f8
bubblestew95 <bubblestew95@gmail.com>
2024-12-27 15:10:25 +0900

    Multiplay 테스트용 씬 구성 완료.

---
+++ Assets/Scripts/Multiplay/NetworkPlayerManager.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+using Photon.Pun;
+
+public class NetworkPlayerManager : MonoBehaviourPun
+{
+    [SerializeField]
+    private GameObject[] destroyObjects = null;
+
+    [SerializeField]
+    private GameObject cameraGo = null;
+
+    private void Start()
+    {
+        if (!photonView.IsMine)
+        {
+            foreach (GameObject go in destroyObjects)
+            {
+                Destroy(go);
+            }
+
+            cameraGo.SetActive(false);
+        }
+    }
+}
--- Assets/Scripts/Puzzle/Interfaces/IPuzzleObject.cs
+++ Assets/Scripts/Puzzle/Interfaces/IPuzzleObject.cs
@@ -1,3 +1,6 @@
+/// <summary>
+/// 퍼즐 오브젝트의 스크립트에서 구현해야 할 인터페이스
+/// </summary>
 public interface IPuzzleObject
 {
     public void SolvePuzzle();
