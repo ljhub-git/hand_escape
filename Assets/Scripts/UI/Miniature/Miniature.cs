7999f59690c77f6f9ba3fd876aea5ed559190118
lordcreep <hjgr.lordcreep@gmail.com>
2024-12-31 20:19:50 +0900

    스냅 적용, 수정필요

--- Assets/Scripts/UI/Miniature/Miniature.cs
+++ Assets/Scripts/UI/Miniature/Miniature.cs
@@ -1,16 +1,50 @@
 using UnityEngine;
-using UnityEngine.XR.Interaction.Toolkit.Interactables;
 
 public class Miniature : MonoBehaviour
 {
-    [SerializeField]
-    private Transform target;
+    [SerializeField] private Transform target;
+    [SerializeField] private float snapThreshold = 30f; // Threshold for snapping (e.g., within 10 degrees)
+    [SerializeField] private float[] snapAngles = { 0f, 90f, 180f, 270f}; // Snap angles
 
-    private Rigidbody rb;
-    private XRGrabInteractable interactable;
+    private float lastSnapAngles = -1f;
 
     private void Update()
     {
+        float currentYRotation = NormalizeAngle(transform.eulerAngles.y);
+        float snappedAngle = GetSnapAngle(currentYRotation);
+
+        if (snappedAngle >= 0)
+        {
+            transform.eulerAngles = new Vector3(0, snappedAngle, 0);
+        }
+        
+        SyncTransform();
+
+    }
+
+    private float GetSnapAngle(float currentYRotation)
+    {
+        foreach (float snapAngle in snapAngles)
+        {
+            //Debug.Log(Mathf.Abs(Mathf.DeltaAngle(currentYRotation, snapAngle)));
+            if (Mathf.Abs(Mathf.DeltaAngle(currentYRotation, snapAngle)) <= snapThreshold && !Mathf.Approximately(lastSnapAngles, snapAngle))
+            {
+                lastSnapAngles = snapAngle;
+                return snapAngle;
+            }
+        }
+        return -1f; // No snapping condition met
+    }
+
+    private float NormalizeAngle(float angle)
+    {
+        angle %= 360f;
+        return angle < 0 ? angle + 360f : angle;
+    }
+
+    private void SyncTransform()
+    {
         target.rotation = transform.rotation;
     }
+
 }
