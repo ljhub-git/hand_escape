e4fc821dc6986e78085039a9281cb6d49b05d243
bubblestew95 <bubblestew95@gmail.com>
2025-01-02 09:22:02 +0900

    테스트중...

--- Assets/Scripts/Multiplay/NetworkPlayerManager.cs
+++ Assets/Scripts/Multiplay/NetworkPlayerManager.cs
@@ -1,10 +1,13 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR;
 
 using Photon.Pun;
+using NUnit.Framework;
 
 public class NetworkPlayerManager : MonoBehaviourPun
 {
+
     [SerializeField]
     private Transform headTr = null;
     [SerializeField]
@@ -18,14 +21,18 @@ public class NetworkPlayerManager : MonoBehaviourPun
     [SerializeField]
     private GameObject cameraGo = null;
 
+    private InputDevice handDevice;
+
     private void Start()
     {
         if (!photonView.IsMine)
         {
-            cameraGo.SetActive(false);
+            foreach(var obj in destroyObjects)
+            {
+                Destroy(obj);
+            }
         }
     }
-
     private void MapPosition(Transform _target, XRNode _node)
     {
         //InputDevices.GetDeviceAtXRNode(_node).TryGetFeatureVal
