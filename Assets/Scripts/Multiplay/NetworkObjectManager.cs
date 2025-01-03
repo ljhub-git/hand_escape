03278de9d6d4bec4973ff952d6cc71d336f93b63
bubblestew95 <bubblestew95@gmail.com>
2025-01-03 17:59:47 +0900

    퍼즐 멀티 대응 작업중.... 집에서 추가작업해라 이놈아.

--- Assets/Scripts/Multiplay/NetworkObjectManager.cs
+++ Assets/Scripts/Multiplay/NetworkObjectManager.cs
@@ -1,7 +1,5 @@
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.XR.Interaction.Toolkit;
-using UnityEngine.XR.Interaction.Toolkit.Interactables;
 
 using Photon.Pun;
 using Photon.Realtime;
@@ -26,33 +24,57 @@ public class NetworkObjectManager : MonoBehaviourPunCallbacks
 
     private void Start()
     {
-        // 씬에 있는 포톤 뷰를 가져오고 포톤뷰 아이디를 통해서 networkInteractableMap 을 초기화한다.
-        {
-            // 포톤 서버에 접속이 되어있어야 함.
-            if (!PhotonNetwork.IsConnected)
-                return;
-
-            // PhotonView 컴포넌트를 가진 오브젝트들을 모두 가져온다.
-            PhotonView[] photonViews = FindObjectsByType<PhotonView>(FindObjectsSortMode.None);
-
-            foreach (var view in photonViews)
-            {
-                networkObjectMap.Add(view.ViewID, view.gameObject);
-            }
-        }
-
-
+        InitObjectMap();
     }
 
-    // 해당 물체의 중력을 모든 클라이언트에서도 설정하도록 한다.
+    // 씬에 있는 포톤 뷰를 가져오고 포톤뷰 아이디를 통해서 networkInteractableMap 을 초기화한다.
+    public void InitObjectMap()
+    {
+        // 포톤 서버에 접속이 되어있어야 함.
+        if (!PhotonNetwork.IsConnected)
+            return;
+
+        // 맵을 초기화한다.
+        networkObjectMap.Clear();
+
+        // PhotonView 컴포넌트를 가진 오브젝트들을 모두 가져온다.
+        PhotonView[] photonViews = FindObjectsByType<PhotonView>(FindObjectsSortMode.None);
+
+        foreach (var view in photonViews)
+        {
+            networkObjectMap.Add(view.ViewID, view.gameObject);
+        }
+    }
+
+    // 해당 물체의 중력 사용여부를 다른 클라이언트에서 설정하도록 한다.
     public void SetNetworkObjectGravityUsable(PhotonView _view, bool _usable)
     {
-        if (!PhotonNetwork.IsConnected || _view == null)
+        if (!IsViewValid(_view))
             return;
 
         int id = _view.ViewID;
 
         photonView.RPC("RPC_SetGravity", RpcTarget.Others, id, _usable);
+    }
+
+    // 퍼즐이 풀렸을 때 반응하는 오브젝트들을 다른 클라이언트에서도 반응하도록 한다.
+    public void CallOnPuzzleSolvedToOthers(PhotonView _view)
+    {
+        if (!IsViewValid(_view) || _view.GetComponent<PuzzleReactObject>() == null)
+            return;
+
+        int id = _view.ViewID;
+
+        photonView.RPC("RPC_PuzzleSolved", RpcTarget.Others, id);
+    }
+
+    // 매개변수로 들어온 뷰가 유효한 뷰인지 체크.
+    private bool IsViewValid(PhotonView _view)
+    {
+        if (!PhotonNetwork.IsConnected || _view == null)
+            return false;
+
+        return true;
     }
 
     #region RPC
@@ -73,5 +95,12 @@ public class NetworkObjectManager : MonoBehaviourPunCallbacks
         networkObjectMap[_viewId].transform.rotation = _rot;
     }
 
+    [PunRPC]
+    private void RPC_PuzzleSolved(int _viewId)
+    {
+        PuzzleReactObject reactObj = networkObjectMap[_viewId].GetComponent<PuzzleReactObject>();
+
+        reactObj?.OnPuzzleSolved();
+    }
     #endregion
 }
