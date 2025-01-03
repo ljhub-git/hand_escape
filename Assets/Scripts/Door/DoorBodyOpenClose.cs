094598927130342d2a36805fba1a4f87be0f9cad
HWANG <jhdsew9471@gmail.com>
2025-01-03 14:07:37 +0900

    Door Open Close Script

--- Assets/Scripts/Door/DoorBodyOpenClose.cs
+++ Assets/Scripts/Door/DoorBodyOpenClose.cs
@@ -1,18 +1,25 @@
 using System.Collections;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class DoorBodyOpenClose : MonoBehaviour
 {
-    private Quaternion openedDoorRot = new Quaternion(0f, 0f, 0f, 0f);
-    private Quaternion closedDoorRot = new Quaternion(0f, 90f, 0f, 0f);
+    private Quaternion openedDoorRot = Quaternion.Euler(0f, 5f, 0f);
+    private Quaternion closedDoorRot = Quaternion.Euler(0f, 90f, 0f);
     private Coroutine DoorMoveCoroutine = null; // 코루틴을 추적할 변수
+    private bool isLocked = false;
+
     public void OpenDoor()
     {
-        if (DoorMoveCoroutine != null)
+        if (DoorMoveCoroutine != null) 
         {
             StopCoroutine(DoorMoveCoroutine);
         }
-        DoorMoveCoroutine = StartCoroutine(DoorMove(openedDoorRot));
+        if (!isLocked)
+        {
+            DoorMoveCoroutine = StartCoroutine(DoorMove(openedDoorRot));
+        }
+
         //transform.rotation = openedDoor;
     }
     public void CloseDoor()
@@ -29,13 +36,18 @@ public class DoorBodyOpenClose : MonoBehaviour
     private IEnumerator DoorMove(Quaternion _goalRot)
     {
         float t = 0;
-        Quaternion curRot = Quaternion.identity;
-        while (true)
+        Quaternion curRot = transform.rotation;
+        while (t < 1)
         {
             curRot = transform.rotation;
-            Quaternion.Lerp(curRot, _goalRot, t);
             t += Time.deltaTime;
+            t = Mathf.Min(t, 1);  // t 값이 1을 넘지 않도록 제한
+            if (t > 0)
+            {
+                transform.rotation = Quaternion.Lerp(curRot, _goalRot, t);
+            }
             yield return null;
         }
+        transform.rotation = _goalRot;
     }
 }
